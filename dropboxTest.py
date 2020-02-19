8b700f13e3e6f1fb6e036c83adda19ddf0232d0c
Joe <joeshimwell@gmail.com>
2020-02-19 15:26:51 +0000

    Time Naming

--- dropboxTest.py
+++ dropboxTest.py
@@ -1,29 +1,31 @@
 import dropbox
 import os
 from config import *
-from time import sleep
+from time import sleep, strftime
 from picamera import PiCamera
 
 dbx = dropbox.Dropbox(YOUR_ACCESS_TOKEN)
+
+file_path = strftime("%Y%m%d-%H%M%S")+"jpg"
 
 camera = PiCamera()
 camera.resolution = (1024, 768)
 camera.start_preview()
 # Camera warm-up time
 sleep(2)
-camera.capture('foo.jpg')
+camera.capture(file_path)
 camera.stop_preview()
 
-with open("foo.jpg", "rb") as f:
-    dbx.files_upload(f.read(), '/foo.jpg', mute = True)
+with open(file_path, "rb") as f:
+    dbx.files_upload(f.read(), file_path, mute = True)
 
-# getting the filename from the user
-file_path = "foo.jpg"
+# # getting the filename from the user
+# file_path = "foo.jpg"
 
-# checking whether file exists or not
-if os.path.exists(file_path):
-    # removing the file using the os.remove() method
-    os.remove(file_path)
-else:
-    # file not found message
-    print("File not found in the directory")
+# # checking whether file exists or not
+# if os.path.exists(file_path):
+#     # removing the file using the os.remove() method
+#     os.remove(file_path)
+# else:
+#     # file not found message
+#     print("File not found in the directory")
