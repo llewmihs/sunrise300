cd82a74a75df1bac54b51626acf742bc68531a77
Joe <joeshimwell@gmail.com>
2020-02-19 16:08:53 +0000

    Test

---
+++ take25.py
@@ -0,0 +1,17 @@
+from time import sleep, strftime
+from picamera import PiCamera
+
+#set up the camera
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+
+# Camera warm-up time
+sleep(2)
+
+for i in range(50):
+    #create timestamp filename
+    file_path = strftime("%Y%m%d-%H%M%S")+".jpg"
+    camera.capture(file_path)
+    print(i)
+    sleep(1)
