b260ad3d08fff57afea4d39517283592a990e39d
Joe <joeshimwell@gmail.com>
2020-02-19 16:24:26 +0000

    lat long

--- getsunny.py
+++ getsunny.py
@@ -1,7 +1,9 @@
 from astral import LocationInfo
 import datetime
 
-city = LocationInfo("Newcastle Upon Tyne", "England", "Europe/London", 51.5, -0.116)
+# l = LocationInfo('name', 'region', 'timezone/name', 0.1, 1.2)
+
+city = LocationInfo("Newcastle Upon Tyne", "England", "Europe/London", 55.0464, 1.4513)
 print((
     f"Information for {city.name}/{city.region}\n"
     f"Timezone: {city.timezone}\n"
