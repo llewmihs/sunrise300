04e46cd3a2ec2ed37161f532673f0a0568b28df8
Joe <joeshimwell@gmail.com>
2020-02-19 15:15:15 +0000

    Changes to filenames in Dropbox

--- dropboxTest.py
+++ dropboxTest.py
@@ -5,5 +5,5 @@ dbx = dropbox.Dropbox(YOUR_ACCESS_TOKEN)
 
 #print(dbx.users_get_current_account())
 
-with open("/Users/joe/Desktop/sunrise300/test.txt", "rb") as f:
-    dbx.files_upload(f.read(), '/test.txt', mute = True)
+with open("test2.txt", "rb") as f:
+    dbx.files_upload(f.read(), '/test2.txt', mute = True)
