37b9686c62d6c4a798981f0f84ecc53df76407dc
Joe <joeshimwell@gmail.com>
2020-02-19 16:25:56 +0000

    astral

--- getsunny.py
+++ getsunny.py
@@ -4,8 +4,15 @@ import datetime
 # l = LocationInfo('name', 'region', 'timezone/name', 0.1, 1.2)
 
 city = LocationInfo("Newcastle Upon Tyne", "England", "Europe/London", 55.0464, 1.4513)
+
+
+from astral.sun import sun
+s = sun(city.observer, date=datetime.date(2009, 4, 22))
+
 print((
-    f"Information for {city.name}/{city.region}\n"
-    f"Timezone: {city.timezone}\n"
-    f"Latitude: {city.latitude:.02f}; Longitude: {city.longitude:.02f}\n"
+    f'Dawn:    {s["dawn"]}\n'
+    f'Sunrise: {s["sunrise"]}\n'
+    f'Noon:    {s["noon"]}\n'
+    f'Sunset:  {s["sunset"]}\n'
+    f'Dusk:    {s["dusk"]}\n'
 ))
