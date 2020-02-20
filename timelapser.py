9d7c361b33fdb6092a14f3d9ae09d7fdaa2cc400
Joe <joeshimwell@gmail.com>
2020-02-20 14:34:21 +0000

    Altohgether

--- timelapser.py
+++ timelapser.py
@@ -1,5 +1,34 @@
 import subprocess
+import dropbox
+import os
+from config import *
 
-output = subprocess.check_output(["ls"],universal_newlines=True)
+from time import sleep, strftime
+from picamera import PiCamera
+
+#set up the camera
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+
+# Camera warm-up time
+sleep(2)
+
+for i in range(50):
+    #create timestamp filename
+    file_path = strftime("%Y%m%d-%H%M%S")+".jpg"
+    camera.capture(file_path)
+    print(i)
+    sleep(1)
+
+subprocess.call(["ls *.jpg > stills.txt"])
+output = subprocess.check_output(["mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 -o timelapse.avi -mf type=jpeg:fps=24 mf://@stills.txt"],universal_newlines=True)
 
 print(output)
+
+subprocess.call(["rm -r *.jpg"])
+
+with open("timelapse.avi", "rb") as f:
+    dbx.files_upload(f.read(), "/timelapse.avi", mute = True)
+
+subprocess.call(["rm -r stills.txt"])
