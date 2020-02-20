1dee8f47b28e9fabf10b7d61a2fb2c91917ababb
Joe <joeshimwell@gmail.com>
2020-02-20 14:35:20 +0000

    16 fps

--- timelapser.py
+++ timelapser.py
@@ -22,7 +22,7 @@ for i in range(50):
     sleep(1)
 
 subprocess.call(["ls *.jpg > stills.txt"])
-output = subprocess.check_output(["mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 -o timelapse.avi -mf type=jpeg:fps=24 mf://@stills.txt"],universal_newlines=True)
+output = subprocess.check_output(["mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 -o timelapse.avi -mf type=jpeg:fps=16 mf://@stills.txt"],universal_newlines=True)
 
 print(output)
 
