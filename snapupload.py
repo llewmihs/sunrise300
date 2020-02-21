6621eb7c883c77e837424dd099b41703d79826e8
Joe <joeshimwell@gmail.com>
2020-02-21 19:56:47 +0000

    snap

---
+++ snapupload.py
@@ -0,0 +1,33 @@
+import dropbox
+import os
+from config import *
+from time import sleep, strftime
+from picamera import PiCamera
+
+# set up dropbox
+dbx = dropbox.Dropbox(YOUR_ACCESS_TOKEN)
+
+#set up the camera
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+
+# Camera warm-up time
+sleep(2)
+
+file_path = strftime("%Y%m%d-%H%M%S")+".jpg"
+    camera.capture(file_path)
+    # upload the file to dropbox
+    with open(file_path, "rb") as f:
+        dbx.files_upload(f.read(), "/" + file_path, mute = True)
+
+    # remove the file once upload complete
+    if os.path.exists(file_path):
+        # removing the file using the os.remove() method
+        os.remove(file_path)
+    else:
+        # file not found message
+        print("File not found in the directory")
+    sleep(2)
+
+camera.stop_preview()
