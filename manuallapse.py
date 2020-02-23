dc09533d339211d94473c7332611d43d01ae9734
Joe <joeshimwell@gmail.com>
2020-02-23 15:43:40 +0000

    aij

--- manuallapse.py
+++ manuallapse.py
@@ -19,20 +19,31 @@ sleep(2)
 
 no_of_pics = 900
 
+print("Taking images...")
 for i in range(no_of_pics):
     #create timestamp filename
     file_path = strftime("%Y%m%d-%H%M%S")+".jpg"
     camera.capture(file_path)
     print(i)
     sleep(1)
+print("Stills complete.")
+
+vid_path = strftime("%Y%m%d-%H%M%S")+".avi"
 
 subprocess.call("ls *.jpg > stills.txt", shell=True)
-subprocess.call("mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 -o timelapse.avi -mf type=jpeg:fps=15 mf://@stills.txt",shell=True)
 
-subprocess.call("rm -r *.jpg", shell=True)
+print("Building video...")
+subprocess.call("mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 -o %s -mf type=jpeg:fps=15 mf://@stills.txt" % vid_path,shell=True)
 
-with open("timelapse.avi", "rb") as f:
-    dbx.files_upload(f.read(), "/timelapse.avi", mute = True)
 
+
+print("Uploading video...")
+with open(vid_path, "rb") as f:
+    dbx.files_upload(f.read(), "/" + vid_path, mute = True)
+
+
+print("Cleaning up...")
 subprocess.call("rm -r stills.txt", shell=True)
 subprocess.call("rm -r *.avi", shell=True)
+subprocess.call("rm -r *.jpg", shell=True)
+print("Fin")
