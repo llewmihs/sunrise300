908442a20fabf28c0d5ef142e892d121533deb32
Joe <joeshimwell@gmail.com>
2020-02-24 22:39:27 +0000

    asd

--- justpics.py
+++ justpics.py
@@ -7,6 +7,10 @@ from picamera import PiCamera
 import sys
 from glob import glob
 
+# mobile notification software
+from notify_run import Notify
+notify = Notify()
+
 # set up dropbox
 dbx = dropbox.Dropbox(YOUR_ACCESS_TOKEN, timeout = None)
 
@@ -29,17 +33,17 @@ total_frames = film_length * frame_rate
 delay = real_time / total_frames
 print("The delay is %d seconds" % delay)
 
+notify.send("Starting to take pictures")
+
 print("Taking images...")
-for i in range(20):
-#for i in range(total_frames):
+for i in range(total_frames):
     #create timestamp filename
     file_path = strftime("%Y%m%d-%H%M%S")+".jpg"
     camera.capture(file_path)
     print(i)
-    sleep(1)
-    #sleep(delay)
+    sleep(delay)
 
-print("Stills complete.")
+notify.send("Starting to take upload")
 
 files = glob('*.jpg')
 
@@ -47,3 +51,4 @@ for i in range(len(files)):
     with open(files[i], "rb") as f:
         dbx.files_upload(f.read(), "/" + files[i], mute = True)
 
+notify.send("Upload complete")
