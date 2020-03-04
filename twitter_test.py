adaa0f59486ea6d9c9cf5b24d7234460fdd9eb90
Joe <joeshimwell@gmail.com>
2020-03-04 10:23:03 +0000

    asd

--- twitter_test.py
+++ twitter_test.py
@@ -4,4 +4,6 @@ from config import *    # my dropbox API key and Push bullet API key
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-print(twitter.verify_credentials())
+video = open('/path/to/file/video.mp4', 'rb')
+response = twitter.upload_video(media=video, media_type='video/mp4')
+twitter.update_status(status='Checkout this cool video!', media_ids=[response['media_id']])
