03b37af140ede36e11a8b1d97b6abfc62949e607
Joe <joeshimwell@gmail.com>
2020-03-04 10:20:53 +0000

    asd

---
+++ twitter_test.py
@@ -0,0 +1,7 @@
+from twython import Twython # pip3 install twython
+
+from config import *    # my dropbox API key and Push bullet API key
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+twitter.verify_credentials()
