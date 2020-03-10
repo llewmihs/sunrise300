fb6d1621b90f175e2d0004c6abe14b2d1e4c3af4
Joe <joeshimwell@gmail.com>
2020-03-10 08:09:01 +0000

    asd

--- sunrise3000.py
+++ sunrise3000.py
@@ -86,6 +86,35 @@ def the_camera(no_of_frames, delay):
 
     camera.stop_preview()
 
+def test_lapser():
+    print("Filename:")
+    video = input()
+    print("Framerate:")
+    frames = input()
+    print("CRF (17-22):")
+    crf = input()
+    print("Preset (slow, veryslow):")
+    preset = input()
+    start_time = time()
+    print(f"Attempting to compile video file - < {video} > - using FFMPEG")
+    print(".........................................................")
+    subprocess.call(f"ffmpeg -y -r {frames} -f image2 -start_number 0000 -i /home/pi/sunrise300/images/IMAGE_%04d.JPG -vcodec libx264 -preset {preset} -crf {crf} {video}", shell=True)
+    end_time = time()
+    elapsed_time_secs = int(end_time - start_time)
+    elapsed_time_mins  = int(elapsed_time_secs / 60)
+    print(f"Programme executed in {elapsed_time_mins} minutes")
+
+    if os.path.exists(video): # has the file been written, and is it of a decent size?
+        print(f"SUCCESS - FFMPEG created the video file: {video}")
+        file_in_mb = int(os.path.getsize(video)/((1024*1024)))
+        print(f"Fileseize: ~ {file_in_mb} Mb")
+        push = pb.push_note(f"FFMPEG Timelapse Successful: {video}", f"Fileseize: {file_in_mb} Mb")
+    else:
+        print(f"ERROR - FFMPEG failed to create the video file: {video}")
+        print(f"Programme exiting early")
+        sys.exit()
+        push = pb.push_note("There was a failure with the FFMPEG lapse.", "Uh oh")
+
 def the_lapser(vid_file, fps):
     video = vid_file
     frames = fps
