ded27437d8c9c3679022f959bb284b02e18650eb
Joe <joeshimwell@gmail.com>
2020-04-05 21:20:59 +0100

    asd

--- sunrise5000.py
+++ sunrise5000.py
@@ -18,21 +18,25 @@ def timelapse_calculations(duration):
     delay_between_images = duration_seconds / total_number_of_frames
     return total_number_of_frames, delay_between_images
 
-def take_a_picture():
+def take_a_picture(num_of_frames, time_between_images):
     print("Taking a photo")
     camera.start_preview()
     sleep(2)
-    file_name = strftime("%H-%M-%S") + ".JPG"
-    full_path = "/home/pi/sunrise300/images/" + file_name
-    camera.capture(full_path)
+    for i in num_of_frames:
+        file_name = 'IMAGE_' '{0:04d}'.format(i)+".JPG"
+        full_path = "/home/pi/sunrise300/images/" + file_name
+        camera.capture(full_path)
+        sleep(time_between_images)
     camera.stop_preview()
-    return full_path, file_name
 
 def  upload_to_dropbox(filepath, filename):
     print("Uploading to Dropbox")
     dropbox_filepath = "/SunriseImages/"+filename
     with open(filepath, "rb") as f:
         dbx.files_upload(f.read(), dropbox_filepath, mute = True)
+
+def ffmpeg_creation():
+    
 
 if __name__ == "__main__":
     i, j = take_a_picture()
