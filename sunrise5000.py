630b241477c9c559cc100b4bc1a65cba05a9d5c2
Joe <joeshimwell@gmail.com>
2020-04-05 21:13:04 +0100

    lapse calcs

---
+++ sunrise5000.py
@@ -0,0 +1,39 @@
+from time import sleep, strftime, time
+from picamera import PiCamera
+from config import *
+import dropbox
+
+#dropbox, timeout=none allows for uploading of larger files without 30second normal timeout
+dbx = dropbox.Dropbox(YOUR_ACCESS_TOKEN, timeout = None) 
+
+# the Picamera, at full resolution
+camera = PiCamera()
+camera.resolution = (3280, 2464)
+
+def timelapse_calculations(duration):
+    duration_seconds = duration * 60
+    film_length_seconds = 30
+    frame_rate = 30
+    total_number_of_frames = film_length_seconds * frame_rate
+    delay_between_images = duration_seconds / total_number_of_frames
+    return total_number_of_frames, delay_between_images
+
+def take_a_picture():
+    print("Taking a photo")
+    camera.start_preview()
+    sleep(2)
+    file_name = strftime("%H-%M-%S") + ".JPG"
+    full_path = "/home/pi/sunrise300/images/" + file_name
+    camera.capture(full_path)
+    camera.stop_preview()
+    return full_path, file_name
+
+def  upload_to_dropbox(filepath, filename):
+    print("Uploading to Dropbox")
+    dropbox_filepath = "/SunriseImages/"+filename
+    with open(filepath, "rb") as f:
+        dbx.files_upload(f.read(), dropbox_filepath, mute = True)
+
+if __name__ == "__main__":
+    i, j = take_a_picture()
+    upload_to_dropbox(i,j)
