289f11275f44fc96605c5bf11d6d9427c758b8d5
Joe <joeshimwell@gmail.com>
2020-04-06 21:04:31 +0100

    asdd

--- sunrise5000.py
+++ sunrise5000.py
@@ -1,47 +1,17 @@
-from time import sleep, strftime, time
-from picamera import PiCamera
-from config import *
-import dropbox
-import subprocess
+from datetime import datetime, timedelta
+from astral import LocationInfo     # to get the location info `pip3 install astral`
+from astral.sun import sun          # to get the sunrise time
 
+def get_sunrise_and_sunset():
+    city = LocationInfo("Whitley Bay", "England", "Europe/London", 55.0464, -1.4513)
+    s = sun(city.observer, date=localize(datetime.date(datetime.now())))
+    print(s['sunrise'])
+    suntime_0 = s['sunrise'] - timedelta(minutes=60)
+    suntime_1 = s['sunrise'] + timedelta(minutes=30)
+    suntime_2 = s['sunset'] - timedelta(minutes=60)
+    suntime_3 = s['sunset'] + timedelta(minutes=30)
+    print(f"0 - {suntime_0}, 1 - {suntime_1}, 2 - {suntime_2}, 3 - {suntime_3}, ")
 
-#dropbox, timeout=none allows for uploading of larger files without 30second normal timeout
-dbx = dropbox.Dropbox(YOUR_ACCESS_TOKEN, timeout = None) 
-
-# the Picamera, at full resolution
-camera = PiCamera()
-camera.resolution = (3280, 2464)
-
-def timelapse_calculations(duration):
-    duration_seconds = duration * 60
-    film_length_seconds = 30
-    frame_rate = 30
-    total_number_of_frames = film_length_seconds * frame_rate
-    delay_between_images = duration_seconds / total_number_of_frames
-    return total_number_of_frames, delay_between_images
-
-def take_a_picture(num_of_frames, time_between_images):
-    print("Taking a photo")
-    camera.start_preview()
-    sleep(2)
-    for i in num_of_frames:
-        file_name = 'IMAGE_' '{0:04d}'.format(i)+".JPG"
-        full_path = "/home/pi/sunrise300/images/" + file_name
-        camera.capture(full_path)
-        sleep(time_between_images)
-    camera.stop_preview()
-
-def  upload_to_dropbox(filepath, filename):
-    print("Uploading to Dropbox")
-    dropbox_filepath = "/SunriseImages/"+filename
-    with open(filepath, "rb") as f:
-        dbx.files_upload(f.read(), dropbox_filepath, mute = True)
-
-def ffmpeg_creation():
-    video_file_name = strftime("%D-%m-%y-%H-%M-%S") + ".mp4"
-    full_video_path = "/home/pi/sunrise300/" + video_file_name
-    subprocess.call(f"/usr/local/bin/ffmpeg -y -r 30 -f image2 -start_number 0000 -i /home/pi/sunrise300/images/IMAGE_%04d.JPG -vf "fade=type=in:duration=1,fade=type=out:duration=1:start_time=29" -vcodec libx264 -preset slow -crf 17 {video_file_name}", shell=True)    
 
 if __name__ == "__main__":
-    i, j = take_a_picture()
-    upload_to_dropbox(i,j)
+    get_sunrise_and_sunset()
