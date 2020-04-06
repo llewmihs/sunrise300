3781a7ea1a42e8821fe0ed39c5201714009dfe15
Joe <joeshimwell@gmail.com>
2020-04-06 21:12:46 +0100

    asd

--- largebox.py
+++ largebox.py
@@ -1,34 +1,46 @@
-import dropbox      # for uploading to dropbox, `pip3 install dropbox`
-from config import *    # my dropbox API key and Push bullet API key
 import os
+from tqdm import tqdm
+import dropbox
+from config import *
 
-dbx = dropbox.Dropbox(YOUR_ACCESS_TOKEN, timeout = None) #dropbox, timeout=none allows for uploading of larger files without 30second normal timeout
-
-file_path = "/home/pi/sunrise300/06April-Apr-04-2.mp4"
-
-f = open(file_path)
-file_size = os.path.getsize(file_path)
-
-CHUNK_SIZE = 4 * 1024 * 1024
-
-if file_size <= CHUNK_SIZE:
-
-    print (dbx.files_upload(f, dest_path))
-
-else:
-
-    upload_session_start_result = dbx.files_upload_session_start(f.read(CHUNK_SIZE))
-    cursor = dropbox.files.UploadSessionCursor(session_id=upload_session_start_result.session_id,
-                                               offset=f.tell())
-    commit = dropbox.files.CommitInfo(path=dest_path)
-
-    while f.tell() < file_size:
-        if ((file_size - f.tell()) <= CHUNK_SIZE):
-            print (dbx.files_upload_session_finish(f.read(CHUNK_SIZE),
-                                            cursor,
-                                            commit))
+def upload(
+    access_token,
+    file_path,
+    target_path,
+    timeout=900,
+    chunk_size=4 * 1024 * 1024,
+):
+    dbx = dropbox.Dropbox(access_token, timeout=timeout)
+    with open(file_path, "rb") as f:
+        file_size = os.path.getsize(file_path)
+        chunk_size = 4 * 1024 * 1024
+        if file_size <= chunk_size:
+            print(dbx.files_upload(f.read(), target_path))
         else:
-            dbx.files_upload_session_append(f.read(CHUNK_SIZE),
-                                            cursor.session_id,
-                                            cursor.offset)
-            cursor.offset = f.tell()
+            with tqdm(total=file_size, desc="Uploaded") as pbar:
+                upload_session_start_result = dbx.files_upload_session_start(
+                    f.read(chunk_size)
+                )
+                pbar.update(chunk_size)
+                cursor = dropbox.files.UploadSessionCursor(
+                    session_id=upload_session_start_result.session_id,
+                    offset=f.tell(),
+                )
+                commit = dropbox.files.CommitInfo(path=target_path)
+                while f.tell() < file_size:
+                    if (file_size - f.tell()) <= chunk_size:
+                        print(
+                            dbx.files_upload_session_finish(
+                                f.read(chunk_size), cursor, commit
+                            )
+                        )
+                    else:
+                        dbx.files_upload_session_append(
+                            f.read(chunk_size),
+                            cursor.session_id,
+                            cursor.offset,
+                        )
+                        cursor.offset = f.tell()
+
+if __name__ == "__main__":
+    upload(YOUR_ACCESS_TOKEN, "/home/pi/sunrise300/06April-Apr-04-2.mp4", "/06April-Apr-04-2.mp4")
