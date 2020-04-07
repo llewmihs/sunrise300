508a38f42c64857ba500239de90d031683148270
Joe <joeshimwell@gmail.com>
2020-04-07 21:59:31 +0100

    asd

--- sunrise5000.py
+++ sunrise5000.py
@@ -1,17 +1,92 @@
 from datetime import datetime, timedelta
 from astral import LocationInfo     # to get the location info `pip3 install astral`
 from astral.sun import sun          # to get the sunrise time
+#from picamera import PiCamera # raspberry pi camera
+import pytz
+utc=pytz.UTC
+
+from time import sleep
+import subprocess
+# the Picamera
+#camera = PiCamera()
+#camera.resolution = (3280, 2464)
 
 def get_sunrise_and_sunset():
-    city = LocationInfo("Whitley Bay", "England", "Europe/London", 55.0464, -1.4513)
-    s = sun(city.observer, date=localize(datetime.date(datetime.now())))
-    print(s['sunrise'])
-    suntime_0 = s['sunrise'] - timedelta(minutes=60)
-    suntime_1 = s['sunrise'] + timedelta(minutes=30)
-    suntime_2 = s['sunset'] - timedelta(minutes=60)
-    suntime_3 = s['sunset'] + timedelta(minutes=30)
-    print(f"0 - {suntime_0}, 1 - {suntime_1}, 2 - {suntime_2}, 3 - {suntime_3}, ")
+    city = LocationInfo("Whitley Bay", "England", "Europe/London", 55.051331, -1.451146)
+    s = sun(city.observer, date=(datetime.date(datetime.now())))
+    # correct for GMT vs UCT
+    sunrise = s['sunrise'] + timedelta(minutes=60)
+    sunset = s['sunset'] + timedelta(minutes=60)
+    return sunrise, sunset
 
+def calculate_the_lapse_windows(sunrise, sunset):
+    #-------T0-----sunrise---------T1-------------T2-----sunset----------T3
+    #---- -1hr --------------- +30mins ------- -1hr --------------- +30mins
+    T0 = sunrise - timedelta(minutes=60)
+    T1 = sunrise + timedelta(minutes=30)
+    T2 = sunset - timedelta(minutes=60)
+    T3 = sunset + timedelta(minutes=30)
+    return T0, T1, T2, T3
+
+def photo_delay_calculations(time_x, time_y, vid_length):
+    frames = vid_length * 30
+    delay = (time_y - time_x)/frames
+    time_string = str(delay)
+    delay = int(time_string[-2:])
+    return delay
+
+def auto_camera(delay, vid_length, folder):
+    print(f"Starting the {folder} timelapse")
+    camera.start_preview()
+    sleep(2) 
+    for i in range(vid_length*30):
+        if i < int(vid_length*30/2):
+            file_path = "/home/pi/sunrise300/" + folder + 'IMAGEA_' + '{0:04d}'.format(i)+".JPG"
+        else:
+            file_path = "/home/pi/sunrise300/" + folder + 'IMAGEB_' + '{0:04d}'.format(i)+".JPG"
+        camera.capture(file_path)
+        sleep(delay)
+    camera.stop_preview()
+    return True
+
+def ffmpeg_creation(folder):
+    filepathA = "/home/pi/sunrise300/" + folder + 'IMAGEA__%04d.JPG'
+    filepathA = "/home/pi/sunrise300/" + folder + 'IMAGEB__%04d.JPG'
+
+    subprocess.call(f"/usr/local/bin/ffmpeg -y -r 30 -f image2 -start_number 0000 -i {filepathA} -vcodec libx264 -preset slow -crf 17 timelapse-a.mp4", shell = True)
+    subprocess.call(f"/usr/local/bin/ffmpeg -y -r 30 -f image2 -start_number 0450 -i {filepathB} -vcodec libx264 -preset slow -crf 17 timelapse-a.mp4", shell = True)
 
 if __name__ == "__main__":
-    get_sunrise_and_sunset()
+    sunrise, sunset = get_sunrise_and_sunset()
+
+    T0, T1, T2, T3 = calculate_the_lapse_windows(sunrise, sunset)
+
+    sunrise_frame_delay = photo_delay_calculations(T0, T1, 30)
+    daytime_frame_delay = photo_delay_calculations(T1, T2, 30)
+    sunset_frame_delay = photo_delay_calculations(T2, T3, 30)
+    
+    run = True
+
+    sunrise_done = False
+    daytime_done = False
+    sunset_done = False
+
+    while run:
+        current_time = utc.localize(datetime.now())
+        if current_time > T0 and current_time < T1 and sunrise_done = False:
+            sunrise_done = auto_camera(sunrise_frame_delay, 30, "sunrise")
+
+        elif current_time > T1 and current_time < T2 daytime_done = False:
+            daytime_done = auto_camera(daytime_frame_delay, 30, "daytime")
+
+        elif current_time > T2 and current_time < T3 sunset_done = False:
+            run = auto_camera(daytime_frame_delay, 30, "sunset")
+        else:
+            pass
+    
+    # if we've got here then we've taken everything for a day
+    # let's ffmpeg!!!
+    
+
+
+            
