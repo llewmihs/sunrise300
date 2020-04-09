0a73d5a600e473b741db8628d7e052b84c346aa7
Joe <joeshimwell@gmail.com>
2020-04-09 23:15:57 +0100

    asd

--- scp-er.py
+++ scp-er.py
@@ -1,6 +1,8 @@
 import subprocess # to run file cleanup after the upload
 from config import *    # my dropbox API key and Push bullet API key
 from time import strftime
+from pushbullet import Pushbullet   # notification software to monitor the programme remotely `pip3 install pushbullet.py`
+pb = Pushbullet(PUSHBULLET)
 
 def rename():
     filename = strftime("%d-%B-%h-%m") + ".mp4"
@@ -15,3 +17,5 @@ def scp_copy(filename, password, localpath):
 if __name__ == "__main__":
     filename = rename()
     scp_copy(filename, password, localpath)
+    push = pb.push_note("Job Done", "Full programme has now run.")
+    
